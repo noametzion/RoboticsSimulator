8eacb4fdc706b954eff4e494b1ba0cd9521dabf4
noam <noametzion@gmail.com>
2017-12-20 01:51:58 +0200

    fix abstract drawing problem

--- src/simulator/LocationAlgorithms/EvaluationShapes/EvaluationShape.java
+++ src/simulator/LocationAlgorithms/EvaluationShapes/EvaluationShape.java
@@ -2,11 +2,20 @@ package simulator.LocationAlgorithms.EvaluationShapes;
 
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.graphics.Color;
+import view.Position;
 import view.SimDrawable;
 
 /**
  * Created by נועם on 12/14/2017.
  */
 public abstract class EvaluationShape implements SimDrawable {
+    @Override
+    public Position getPosition() {
+        return null;
+    }
 
+    @Override
+    public void draw(PaintEvent e, int x, int y, int r, int x0, int y0) {
+
+    }
 }
