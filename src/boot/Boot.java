098a115ecfafa3d76751f3a743154743e19bdc55
noam <noametzion@gmail.com>
2017-12-20 23:10:55 +0200

    fix not drawing problem
    * rebuild fixed it :( :( :(

--- src/boot/Boot.java
+++ src/boot/Boot.java
@@ -1,33 +1,33 @@
-//package boot;
-//
-////import simulator.MBDtest;
-//import simulator.PerimeterDefenseSimTask;
-//import utils.ConvexPolygon2D;
-//import utils.PolygonIntersection;
-//import view.Position;
-//import view.SimulatorWindow;
-//
-//import java.util.ArrayList;
-//
-//public class Boot {
-//
-//
-//	// test
-//	public static void main(String[] args) {
-//		//MBDtest.reset();
-//		SimulatorWindow sw=new SimulatorWindow(800, 500, "my simulator",
-//              new PerimeterDefenseSimTask("scenarios/sc9_1.txt",0,0,1));
-//		sw.start();
-//		try {
-//			sw.join();
-//			//MBDtest.addScores();
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		//MBDtest.printDiagnoses(0.0);
-//
-//
-//
-//    }
-//}
+package boot;
+
+//import simulator.MBDtest;
+import simulator.PerimeterDefenseSimTask;
+import utils.ConvexPolygon2D;
+import utils.PolygonIntersection;
+import view.Position;
+import view.SimulatorWindow;
+
+import java.util.ArrayList;
+
+public class Boot {
+
+
+	// test
+	public static void main(String[] args) {
+		//MBDtest.reset();
+		SimulatorWindow sw=new SimulatorWindow(800, 500, "my simulator",
+              new PerimeterDefenseSimTask("scenarios/sc9_1.txt",10,0,1));
+		sw.start();
+		try {
+			sw.join();
+			//MBDtest.addScores();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//MBDtest.printDiagnoses(0.0);
+
+
+
+    }
+}
